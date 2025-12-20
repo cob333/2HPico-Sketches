58e4aea41c8d14762e7a7d0d61742e8f1f15f9c9
rheslip <rheslip@hotmail.com>
2025-12-20 11:01:38 -0500

    Updated 2HPico lib
    Updated 2hPicolib to reduce duplicated code across apps

--- ADSR/ADSR.ino
+++ ADSR/ADSR.ino
@@ -28,67 +28,38 @@
 // uses DaisySP ADSR 
 // RH Nov 2025
 
-#include <2HPico_io.h>
+#include <2HPico.h>
 #include <I2S.h>
 #include <Adafruit_NeoPixel.h>
 #include "pico/multicore.h"
 
-#define MONITOR_CPU1  // define to enable 2nd core monitoring
+#define DEBUG   // comment out to remove debug code
+
+#define MONITOR_CPU1  // define to enable 2nd core monitoring on CPU_USE pin
 
 //#define SAMPLERATE 11025 
 #define SAMPLERATE 22050 // 
 //#define SAMPLERATE 44100
 
-#define NUMPIXELS 1 // 
-#define RED 0x1f0000  // only using 5 bits to keep LEDs from getting too bright
-#define GREEN 0x001f00
-#define BLUE 0x00001f
-#define ORANGE (RED|GREEN)
-#define VIOLET (RED|BLUE)
-#define AQUA (GREEN|BLUE)
 
 Adafruit_NeoPixel LEDS(NUMPIXELS, LEDPIN, NEO_GRB + NEO_KHZ800);
 
 I2S DAC(OUTPUT);  // using once channel of a PT8211 stereo DAC
 
-#define DEBUG   // comment out to remove debug code
-
-// constants for integer to float and float to integer conversion
-#define MULT_16 2147483647
-#define DIV_16 4.6566129e-10
-
 #include "daisysp.h"
 
-// including the source files is a pain but that way you compile in only the modules you need
-// DaisySP statically allocates memory and some modules e.g. reverb use a lot of ram
+// including the DaisySP source files is a pain but that way you compile in only the modules you need
+// DaisySP statically allocates memory and some modules e.g. reverb use a lot of ram so don't compile them unless you need them
 #include "control/adsr.cpp"
 
 float samplerate=SAMPLERATE; // foar DaisySP
 
-#define GATE TRIGGER    // semantics: ADSR generally used with a gate signal
+#define GATE TRIGGER    // semantics - ADSR is generally used with a gate signal
 // create daisySP processing objects
 Adsr env;
 
-// a/d values from pots
-// the pots are "locked" when the parameter page changes
-// this prevents an immediate change when we switch pages
-// we unlock each pot and allow parameter values to change when there is a significant movement of the pot
-
-#define AD_BITS 12
-#define AD_RANGE 4096  // RP2350 has 12 bit A/D so lets use it
-#define POT1_2  AIN3  // Pots 1 & 2 together on mux
-#define POT3_4  AIN2  // Pots 3 & 4 ""
-#define MIN_POT_CHANGE 100 // pot reading must change by this in order to register
-#define POT_AVERAGING 5  // A/D average over this many readings
-#define NUMPOTS 4
-uint16_t pot[NUMPOTS]; // pot A/D readings
-bool potlock[NUMPOTS]; // when pots are locked it means they must change by MIN_POT_CHANGE to register
-
-#define CVIN_VOLT 580.6  // a/d count per volt - **** adjust this value for more accurate V/octave input
-#define CV1IN AIN0   // CV1 input - top jack
-#define CV2IN AIN1   // CV2 input - middle jack
-#define CV_AVERAGING 10  // A/D average over this many readings
-#define CVOUT_VOLT 6554 // D/A count per volt - nominally +-5v range for -+32767 DAC values- ***** adjust this value for accurate V/octave out
+#define CVIN_VOLT 580.6  // a/d count per volt - **** adjust this value to calibrate V/octave input
+#define CVOUT_VOLT 6554 // D/A count per volt - nominally +-5v range for -+32767 DAC values- ***** adjust this value to calibrate V/octave out
 #define CVOUTMIN -2*CVOUT_VOLT  // lowest output CV ie MIDI note 0
 
 bool gate=0;
@@ -97,54 +68,6 @@ bool button=0;
 enum UIstates {SET1} ;
 uint8_t UIstate=SET1;
 uint32_t buttontimer,gatetimer,parameterupdate;
-#define DEBOUNCE 10
-#define GATE_DEBOUNCE 1  // some modules output a very short trigger
-#define PARAMETERUPDATE 100  // modal doesn't like values that jump around a lot so limit the changes
-
-// sample the pots. potlock means apply hysteresis so we only change when the pot is moved significantly
-void samplepots(void) {
-  for (int i=0; i<NUMPOTS;++i) {
-    uint val=0;
-    digitalWrite(MUXCTL,!(i&1)); // set analog mux to correct pot
-    for (int j=0; j<POT_AVERAGING;++j) val+=analogRead(POT1_2-((i&2)>>1)); // read the A/D a few times and average for a more stable value
-    val=val/POT_AVERAGING;
-    if (potlock[i]) {
-      int delta=pot[i]-val;  // this needs to be done outside of the abs() function - see arduino abs() docs
-      if (abs(delta) > MIN_POT_CHANGE) {
-        potlock[i]=0;   // flag pot no longer locked
-        pot[i]=val; // save the new reading
-      }
-    }
-    else pot[i]=val; // pot is unlocked so save the reading
-  }
-}
-
-// lock all pots. when locked they have to move significantly to change value. 
-// prevents immediately setting new values when parameter set is changed with front panel button
-void lockpots(void) {
-  for (int i=0; i<NUMPOTS;++i) potlock[i]=1;
-}
-
-// sample the CV1 input. 
-uint16_t sampleCV1(void) {
-  uint16_t val=0;
-  for (int16_t j=0; j<CV_AVERAGING;++j) val+=analogRead(CV1IN); // read the A/D a few times and average for a more stable value
-  val=val/CV_AVERAGING;
-  return val;
-}
-
-// sample the CV2 input. 
-uint16_t sampleCV2(void) {
-  uint16_t val=0;
-  for (int16_t j=0; j<CV_AVERAGING;++j) val+=analogRead(CV2IN); // read the A/D a few times and average for a more stable value
-  val=val/CV_AVERAGING;
-  return val;
-}
-
-// like the map() function but maps to float values
-float mapf(long x, long in_min, long in_max, float out_min, float out_max) {
-  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-}
 
 void setup() { 
   Serial.begin(115200);
@@ -165,11 +88,6 @@ void setup() {
   env.Init(samplerate);       // initialize the envelope generator
 
   analogReadResolution(AD_BITS); // set up for max resolution
-// initialize the pot readings
-  for (int16_t i=0; i<NUMPOTS;++i) {
-    pot[i]=0;
-    potlock[i]=0;
-  }
 
   LEDS.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
   LEDS.setPixelColor(0, RED); 
---
+++ lib/2HPicolib/2HPico.h
@@ -0,0 +1,122 @@
+// Copyright 2025 Rich Heslip
+//
+// Author: Rich Heslip 
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+// 
+// See http://creativecommons.org/licenses/MIT/ for more information.
+//
+// -----------------------------------------------------------------------------
+//
+// misc defines and support routines for the 2HPico module
+// R Heslip Dec 2025
+
+#include "2HPico_io.h"
+#include "ClickButton.h"
+
+// for Adafruit Neopixels
+#define NUMPIXELS 1 // 
+#define RED 0x1f0000  // only using 5 bits to keep LEDs from getting too bright
+#define GREEN 0x001f00
+#define BLUE 0x00001f
+#define ORANGE (RED|GREEN)
+#define VIOLET (RED|BLUE)
+#define AQUA (GREEN|BLUE)
+
+// constants for integer to float and float to integer conversion
+#define MULT_16 2147483647
+#define DIV_16 4.6566129e-10
+
+
+#define DEBOUNCE 10   // debounce for buttons
+#define GATE_DEBOUNCE 1  // some modules output a very short trigger so don't do a long debounce
+#define TRIG_DEBOUNCE 1  // short trigger debounce too
+#define CLOCK_DEBOUNCE 1 // short clock debounce
+#define PARAMETERUPDATE 100  // some DaisySP models don't like values that jump around a lot so limit the changes
+
+// A/D values from CV inputs
+
+#define CV1IN AIN0   // CV1 input - top jack
+#define CV2IN AIN1   // CV2 input - middle jack
+#define CV_AVERAGING 10  // A/D average over this many readings
+
+// a/d values from pots
+// the pots are "locked" when the parameter page changes
+// this prevents an immediate change when we switch pages
+// we unlock each pot and allow parameter values to change when there is a significant movement of the pot
+
+#define AD_BITS 12
+#define AD_RANGE 4096  // RP2350 has 12 bit A/D so lets use it
+#define POT1_2  AIN3  // Pots 1 & 2 together on mux
+#define POT3_4  AIN2  // Pots 3 & 4 ""
+#define MIN_POT_CHANGE 100 // pot reading must change by this in order to register
+#define POT_AVERAGING 5  // A/D average over this many readings
+#define NUMPOTS 4
+
+
+// Pot and CV sampling routines
+
+uint16_t pot[NUMPOTS]; // pot A/D readings
+bool potlock[NUMPOTS]; // when pots are locked it means they must change by MIN_POT_CHANGE to register
+
+
+// sample the pots. potlock means apply hysteresis so we only change when the pot is moved significantly
+void samplepots(void) {
+  for (int i=0; i<NUMPOTS;++i) {
+    uint val=0;
+    digitalWrite(MUXCTL,!(i&1)); // set analog mux to correct pot
+    for (int j=0; j<POT_AVERAGING;++j) val+=analogRead(POT1_2-((i&2)>>1)); // read the A/D a few times and average for a more stable value
+    val=val/POT_AVERAGING;
+    if (potlock[i]) {
+      int delta=pot[i]-val;  // this needs to be done outside of the abs() function - see arduino abs() docs
+      if (abs(delta) > MIN_POT_CHANGE) {
+        potlock[i]=0;   // flag pot no longer locked
+        pot[i]=val; // save the new reading
+      }
+    }
+    else pot[i]=val; // pot is unlocked so save the reading
+  }
+}
+
+// lock all pots. when locked they have to move significantly to change value. 
+// prevents immediately setting new values when parameter set is changed with front panel button
+void lockpots(void) {
+  for (int i=0; i<NUMPOTS;++i) potlock[i]=1;
+}
+
+// sample the CV1 input. 
+uint16_t sampleCV1(void) {
+  uint16_t val=0;
+  for (int16_t j=0; j<CV_AVERAGING;++j) val+=analogRead(CV1IN); // read the A/D a few times and average for a more stable value
+  val=val/CV_AVERAGING;
+  return val;
+}
+
+// sample the CV2 input. 
+uint16_t sampleCV2(void) {
+  uint16_t val=0;
+  for (int16_t j=0; j<CV_AVERAGING;++j) val+=analogRead(CV2IN); // read the A/D a few times and average for a more stable value
+  val=val/CV_AVERAGING;
+  return val;
+}
+
+// like the map() function but maps to float values
+float mapf(long x, long in_min, long in_max, float out_min, float out_max) {
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
